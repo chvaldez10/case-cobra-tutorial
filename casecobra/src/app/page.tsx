ddf73c1f238c750a3fdb2d8d99a85e0acf38769b
chvaldez10 <christianvaldez021@gmail.com>
2024-08-17 16:02:57 -0600

    setup

---
+++ casecobra/src/app/page.tsx
@@ -0,0 +1,10 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bg-slate-50">
+      <section></section>
+    </div>
+  );
+}
